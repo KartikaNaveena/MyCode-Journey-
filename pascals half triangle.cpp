9fe794c1d69a267a329bb01dac4d223f28b6b288
KartikaNaveena <115171789+KartikaNaveena@users.noreply.github.com>
2022-12-04 12:26:52 +0530

    Add files via upload

---
+++ pascals half triangle.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int i,j,k=1;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(j<=i)
+			{
+				cout<<k;
+				k=k*(i-j)/j;
+			}
+			else
+			{
+				cout<<"\n";
+			}
+		
+		}
+	k=1;
+	}
+}
