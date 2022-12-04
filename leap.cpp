a01e1115f9315afaf08e6591131b59348a5a4c4e
KartikaNaveena <115171789+KartikaNaveena@users.noreply.github.com>
2022-12-04 12:30:22 +0530

    Add files via upload

---
+++ leap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int product,n;
+	cin>>n;
+	if((n%400==0)||((n%4==0) && (n%100!=0)))
+	{
+		cout<<"yes";
+	}
+	else
+	cout<<"no";
+	return 0;
+}
